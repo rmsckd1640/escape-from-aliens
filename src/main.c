82560b420a0f418d0388f4e581492912af4c0381
jangdonghyun <dongza8101@naver.com>
2025-05-28 16:37:51 +0900

    item-score: WIP: 작업 중간 저장

--- src/main.c
+++ src/main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include "enemy.h"
 #include "map.h"
+#include "item.h"
 
 int main(void) {
 
     printf("Hello, World!\n");
     enemy();
-
+    item();
     map();
 
     getchar();
