21779dcd878658e8026c887a42c81b18cd470a21
ppaangss <ppaangss@naver.com>
2025-06-03 23:15:24 +0900

    enemy: feat: 플레이어 적 충돌  판정 기능 구현

--- include/battle.h
+++ include/battle.h
@@ -1,1 +1,9 @@
-﻿
+﻿#include "player.h"
+
+#define ENEMY_DAMAGE 1 // 닿았을 때 입히는 데미지
+
+// 적이 플레이어에게 닿았는지 검사하고 데미지를 입힌다.
+// @param enemies: 적 배열
+void checkEnemyCollision(PlayerHP* playerHp, const Position* playerPos, Enemy* enemies, int enemyCount);
+
+
