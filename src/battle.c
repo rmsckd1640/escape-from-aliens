3f91ffabfff7285f224b3279df3d9749236fff87
ppaangss <ppaangss@naver.com>
2025-06-06 14:27:00 +0900

    enemy: chore: player관련 battle 수정

--- src/battle.c
+++ src/battle.c
@@ -4,14 +4,14 @@
 //적 충돌 시 player hp 깎임.
 //추후 player 구조체 변경 시 수정.
 
-void checkEnemyCollision(PlayerHP* playerHp, const Position* playerPos, Enemy* enemies, int enemyCount) {
+void checkEnemyCollision(Player *p, Enemy* enemies, int enemyCount) {
     for (int i = 0; i < MAX_ENEMIES; i++) {
         if (!enemies[i].active) continue;//적 없으면 건너뜀.
 
-        if (enemies[i].x == playerPos->x && enemies[i].y == playerPos->y) {//좌표 같을 시 player 데미지 받으며 enemy 없어짐.
-            takeDamage(playerHp, 1);
+        if (enemies[i].x == p->x && enemies[i].y == p->y) {//좌표 같을 시 player 데미지 받으며 enemy 없어짐.
+            takeDamage(p, 1);
             enemies[i].active = 0;//enemy 없어짐.
-            printf(">> 적과 충돌! HP -(%d/%d)\n", playerHp->hp, playerHp->max_hp);
+            printf(">> 적과 충돌! HP -(%d/%d)\n", p->hp, p->max_hp);
         }
     }
 }
