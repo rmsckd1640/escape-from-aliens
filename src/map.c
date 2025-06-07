19bf8b67c02b03b851940a065881e6a76a8b7513
rmsckd1640 <rmsckd1640@gmail.com>
2025-06-07 14:06:13 +0900

    map: feat: 아이템, 플레이어, HP 출력 및 충돌 시 효과 적용

--- src/map.c
+++ src/map.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "map.h"
 #include "enemy.h"  // 적 정보 사용을 위해 추가
+#include "player.h"
+#include "item.h"
 
 void initMap(void) {
     for (int y = 0; y < MAP_HEIGHT; y++) {
@@ -20,21 +22,38 @@ void initMap(void) {
     }
 }
 
+extern Player p;     // 다른 모듈에서 선언된 전역 플레이어 구조체
+
 void drawMap(void) {
     initMap();
-    // 적 그리기
+
+    // 1. 적 출력
     for (int i = 0; i < MAX_ENEMIES; i++) {
         if (enemies[i].active) {
-            map[enemies[i].y][enemies[i].x] = 'E';  // 적을 'E'로 표시
+            map[enemies[i].y][enemies[i].x] = 'E';
         }
     }
 
+    // 2. 아이템 출력
+    drawItems(); // map에 H 또는 B 심볼을 표시
+
+    // 3. 플레이어 출력
+    drawPlayer(&p, map); // 플레이어 위치에 '*' 표시
+
+    // 4. 맵 출력
     for (int y = 0; y < MAP_HEIGHT; y++) {
         for (int x = 0; x < MAP_WIDTH; x++) {
             putchar(map[y][x]);
         }
         putchar('\n');
     }
+
+    // 5. 플레이어 HP 출력
+    printHPBar(&p);  // ♥♥♡ 형태로 HP 바 출력
+
+    // 6. 아이템 충돌 체크 (아이템을 먹었는지 검사하고 효과 적용)
+    checkItemCollision(p.x, p.y, &p);
+
 }
 
 void setTile(int x, int y, char tile) {
